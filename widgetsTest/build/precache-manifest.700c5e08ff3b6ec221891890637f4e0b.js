0e0c4c13600e81b90450bd1475292f67ef90b35e
cathysandstrom <cms@rlsand.com>
2019-08-22 21:46:01 -0700

    temporarily undid changes

---
+++ widgetsTest/build/precache-manifest.700c5e08ff3b6ec221891890637f4e0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ddf04c324176131b4a6f893bf4d96e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44f0d683dc139e3e1f49",
+    "url": "/static/css/main.049df1de.chunk.css"
+  },
+  {
+    "revision": "405adddd93981b2ebabf",
+    "url": "/static/js/2.c2adcf96.chunk.js"
+  },
+  {
+    "revision": "44f0d683dc139e3e1f49",
+    "url": "/static/js/main.dbd4eaac.chunk.js"
+  },
+  {
+    "revision": "a9420df9b50ed7f6e8cf",
+    "url": "/static/js/runtime~main.39e255cb.js"
+  }
+]);
