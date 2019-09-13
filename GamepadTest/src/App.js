e069125df39c932865e3ae62cdbac166505ed24d
arbiter487 <arbiters487@gmail.com>
2019-09-13 17:47:14 -0400

    added circle

--- GamepadTest/src/App.js
+++ GamepadTest/src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.example()
+  }
+
+  state = {
+    y: 50,
+    x: 50
+  }
+
+  example() {
+    this.setState({y: 1})
+    this.setState({x: 1})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="Circle" style={{top: this.state.y+"px",left:this.state.x+"px"}} />
+      </div>
+    );
+  }
+  
 }
 
 export default App;
