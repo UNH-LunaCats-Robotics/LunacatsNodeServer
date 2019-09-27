ed82e2aa8d0507fb923d350b63d483132e0ef19a
cathysandstrom <cms@rlsand.com>
2019-09-27 11:41:46 -0700

    added basic controller interface

--- main/src/ControlDisplay/Controller.js
+++ main/src/ControlDisplay/Controller.js
@@ -6,23 +6,46 @@ class Controller extends React.Component {
         leftX: 20,
         leftY: 20,
         rightX: 20,
-        rightY: 20
+        rightY: 20,
+        y: "lightgray",
+        x: "lightgray",
+        a: "lightgray",
+        b: "lightgray"
+    }
+
+    setButtonColor(buttonName, color) {
+        if(buttonName === 'A') this.setState({ y: color});
+        if(buttonName === 'Y') this.setState({x: color});
+        if(buttonName === 'X') this.setState({a: color});
+        if(buttonName === 'B') this.setState({b: color});
+        
     }
 
     //LeftStickX, LeftStickY, RightStickX, RightStickY
-    axisChangeHandler(axisName, value, previousValue, env) {
+    axisChangeHandler(axisName, value, previousValue) {
         if(axisName === 'LeftStickX') {
-            env.setState({ leftX: (value*15) + 20})
+            this.setState({ leftX: (value*15) + 20})
         }
         if(axisName === 'LeftStickY') {
-            env.setState({ leftY: -(value*15)+20})
+            this.setState({ leftY: -(value*15)+20})
         }
         if(axisName === 'RightStickX') {
-            env.setState({ rightX: (value*15) + 20})
+            this.setState({ rightX: (value*15) + 20})
         }
         if(axisName === 'RightStickY') {
-            env.setState({ rightY: -(value*15)+20})
+            this.setState({ rightY: -(value*15)+20})
         }
+    }
+
+    //NOTE: Default Button Names do not match buttons on controller
+    buttonDownHandler(buttonName) {
+        console.log(buttonName, "down");
+        this.setButtonColor(buttonName, "gray");
+    }
+
+    buttonUpHandler(buttonName) {
+        console.log(buttonName, "up");
+        this.setButtonColor(buttonName, "lightgray");
     }
 
     connectHandler(gamepadIndex) {
@@ -35,21 +58,46 @@ class Controller extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className="d-inline-flex flex-row p-2" style={{backgroundColor: "yellow"}}>
                 <Gamepad
                     onConnect={this.connectHandler}
                     onDisconnect={this.disconnectHandler}
 
                     onAxisChange={(axisName, value, previousValue) => {
-                        this.axisChangeHandler(axisName, value, previousValue, this)
+                        this.axisChangeHandler(axisName, value, previousValue)
+                    }}
+
+                    onButtonDown={(buttonName) => {
+                        this.buttonDownHandler(buttonName)
+                    }}
+
+                    onButtonUp={(buttonName) => {
+                        this.buttonUpHandler(buttonName)
                     }}
                 ><p></p></Gamepad>
-                <div className = "JoystickRim">
+                <div className = "JoystickRim m-2">
                     <div className="JoystickCircle" style={{ top: this.state.leftY + "px", left: this.state.leftX + "px" }}/>
                 </div>
-                <div className = "JoystickRim">
+                
+                <div className = "JoystickButton align-self-center p-0 ml-2" style={{backgroundColor: this.state.y}}>
+                    <p className = "pt-1">Y</p>
+                </div>
+                <div className = "col my-2">
+                    <div className = "JoystickButton p-0 row" style={{backgroundColor: this.state.x}}> 
+                        <p className = "pt-1 ml-2 pl-1">X</p>
+                    </div>
+                    <div className = "JoystickButton p-0 row mt-4" style={{backgroundColor: this.state.b}}>
+                        <p className = "pt-1 ml-2 pl-1">B</p>
+                    </div>
+                </div>
+                <div className = "JoystickButton align-self-center mr-2" style={{backgroundColor: this.state.a}}> 
+                    <p className = "pt-1">A</p>
+                </div>
+
+                <div className = "JoystickRim m-2">
                     <div className="JoystickCircle" style={{ top: this.state.rightY + "px", left: this.state.rightX + "px" }}/>
                 </div>
+                
             </div>
         )
     }
