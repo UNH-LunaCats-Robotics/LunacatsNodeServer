5a76d24478517c9b46355eef0b017979f32a8bc6
Noah <njv1001@wildcats.unh.edu>
2019-10-18 13:24:22 -0400

    Improved Socket Connectiong + Angle Calculation
    The server now supports the client subscribing to timed updates as well as sending events on button press/release. The angle of the joystick is now calculated and sent to the client.

--- main/src/ControlDisplay/Controller.js
+++ main/src/ControlDisplay/Controller.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Gamepad from 'react-gamepad'
+import { subscribeToTimer, startTimer } from '.././api';
+import { sendEvent } from '.././api';
 
 class Controller extends React.Component {
     state = {
@@ -10,15 +12,54 @@ class Controller extends React.Component {
         y: "lightgray",
         x: "lightgray",
         a: "lightgray",
-        b: "lightgray"
+        b: "lightgray",
+        time: 'no timestamp yet'
     }
+
+    getAngle(xL, yL) {
+        if(xL === -0)
+            xL = 0;
+        if(yL === -0)
+            yL = 0;
+
+        //Get the angle relative to x and y
+        var angle = 0;
+        if(xL !== 0) 
+            angle = (180 / Math.PI) * Math.atan(yL / xL);
+        else if(yL < 0) 
+            angle = 270;
+        else if(yL > 0) 
+            angle = 90;
+
+        //Offset angle based on quadrant
+        if(xL < 0)
+            angle = angle + 180;
+        else if(xL > 0 && yL < 0)
+            angle = angle + 360;
+
+        //Get rid of -0
+        if(angle === -0)
+            angle = 0;
+
+        return angle;
+    }
+
+    constructor(props) {
+        super(props);
+
+        /*subscribeToTimer((err, timestamp) => {
+            this.setState({ time: timestamp });
+            console.log(timestamp);
+        });*/
+
+        //startTimer(1000);
+      }
 
     setButtonColor(buttonName, color) {
         if(buttonName === 'A') this.setState({ y: color});
         if(buttonName === 'Y') this.setState({x: color});
         if(buttonName === 'X') this.setState({a: color});
         if(buttonName === 'B') this.setState({b: color});
-        
     }
 
     //LeftStickX, LeftStickY, RightStickX, RightStickY
@@ -35,17 +76,44 @@ class Controller extends React.Component {
         if(axisName === 'RightStickY') {
             this.setState({ rightY: -(value*15)+20})
         }
+        var xL;
+        var yL;
+
+        if(axisName === 'LeftStickX' || axisName === 'LeftStickY') {
+            xL = this.state.leftX - 20;
+            yL = -(this.state.leftY - 20);
+        }
+        else {
+            xL = this.state.rightX - 20;
+            yL = -(this.state.rightY - 20);
+        }
+        
+        var angle = this.getAngle(xL, yL);
+
+        //How far the joystick is tilted
+        var intensity = Math.max(xL, yL);
+        var pushed = 0;
+        if(intensity > 0)
+            pushed = 1;
+    
+        console.log("Angle: ", angle, " Intensity: ", intensity);
+        if(axisName === 'LeftStickX' || axisName === 'LeftStickY')
+            sendEvent('joystick', "L:" + angle + ":" + pushed);
+        else
+            sendEvent('joystick', "R:" + angle + ":" + pushed);
     }
 
     //NOTE: Default Button Names do not match buttons on controller
     buttonDownHandler(buttonName) {
         console.log(buttonName, "down");
         this.setButtonColor(buttonName, "gray");
+        sendEvent('button', buttonname + ":" + 1);
     }
 
     buttonUpHandler(buttonName) {
         console.log(buttonName, "up");
         this.setButtonColor(buttonName, "lightgray");
+        sendEvent('button', buttonname + ":" + 0);
     }
 
     connectHandler(gamepadIndex) {
--- main/src/api.js
+++ main/src/api.js
@@ -1,9 +1,26 @@
 import openSocket from 'socket.io-client';
-const socket = openSocket('http://localhost:3002');
+//const socket = openSocket('http://localhost:3002');
+const socket = openSocket('http://10.0.10.14:3002');
 
 function subscribeToTimer(cb) {
+    console.log("hello");
     socket.on('timer', timestamp => cb(null, timestamp));
     socket.emit('subscribeToTimer', 1000);
 }
 
+function startTimer(interval) {
+    console.log("Timer started. Commands being sent every ", interval);
+    setInterval(() => {
+        socket.emit('event', 0);
+    }, interval);
+}
+
+function sendEvent(e, v) {
+    console.log("Event: " + e);
+    console.log("Value: " + v);
+    socket.emit(e, v);
+}
+
 export {subscribeToTimer}
+export {startTimer}
+export {sendEvent}
